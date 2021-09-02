ef6caf4cb9ebea20feb1925b02d83fb62778e716
ginstone <hjg719@139.com>
2021-09-02 09:56:25 +0800

    杂项

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import store from './store'
 
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './assets/js/utils'
 
 createApp(App)
     .use(store)
     .use(router)
-    .use(ElementPlus)
+    .use(ElementPlus, {locale: zhCn,})
     .mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore } from 'vuex'
 import user from "@/store/user";
+import library from "@/store/library";
+import threadList from "@/store/threadList";
 
 export default createStore({
   state: {
@@ -9,6 +11,6 @@ export default createStore({
   actions: {
   },
   modules: {
-    user,
+    user,library,threadList
   }
 })
